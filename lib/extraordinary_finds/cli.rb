f46af8380bdda0400cdb69989fdbadfb8e2e87f6
Victoria Hale <vic.hale74@gmail.com>
2019-10-29 17:57:24 +0000

    modified lib

--- lib/extraordinary_finds/cli.rb
+++ lib/extraordinary_finds/cli.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class ExtraordinaryFinds::CLI
+  def call 
+    puts "Today's Extraordinary Finds:"
+    list_finds
+    binding.pry
+    menu
+  end
+  
+  def list_finds
+    puts 
+    1.
+    2.
+    3.
+    4.
+    5.
+    6.
+    7.
+    8.
+    9.
+    10.
+  end
+  def menu
+    puts "Enter the number of the find you'd like more information for. Type list to see the list again, or exit."
+    #some code here to reposnd to user input
+  end 
   
 end
