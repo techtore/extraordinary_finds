8283db99f86bfe5c224eba3aca355629e62ef027
Victoria Hale <vic.hale74@gmail.com>
2019-10-29 19:22:09 +0000

    modified lib

--- lib/extraordinary_finds/cli.rb
+++ lib/extraordinary_finds/cli.rb
@@ -3,27 +3,29 @@ require 'pry'
 class ExtraordinaryFinds::CLI
   def call 
     puts "Today's Extraordinary Finds:"
-    list_finds
-    binding.pry
-    menu
+    # list_finds
+    # menu
+    
+    html = open("https://www.amazon.com/")
+    doc = Nokogiri::HTML(html)
   end
   
-  def list_finds
-    puts 
-    1.
-    2.
-    3.
-    4.
-    5.
-    6.
-    7.
-    8.
-    9.
-    10.
-  end
-  def menu
-    puts "Enter the number of the find you'd like more information for. Type list to see the list again, or exit."
-    #some code here to reposnd to user input
-  end 
+#   def list_finds
+#     puts 
+#     1.
+#     2.
+#     3.
+#     4.
+#     5.
+#     6.
+#     7.
+#     8.
+#     9.
+#     10.
+#   end
+#   def menu
+#     puts "Enter the number of the find you'd like more information for. Type list to see the list again, or exit."
+#     #some code here to reposnd to user input
+#   end 
   
-end
+# end
