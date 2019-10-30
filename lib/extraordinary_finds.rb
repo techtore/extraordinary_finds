9755a209c3f86829f034cd360763e8cc8059935f
Victoria Hale <vic.hale74@gmail.com>
2019-10-30 18:23:45 +0000

    modified

--- lib/extraordinary_finds.rb
+++
@@ -1,8 +0,0 @@
-require "extraordinary_finds/version"
-
-module ExtraordinaryFinds
-  class Error < StandardError; end
-  # Your code goes here...
-end
-require "./extraordinary_finds/finds"
-require "./extraordinary_finds/cli"
