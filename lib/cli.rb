3785c16840d41c5ad132e5424b5dbe27091bb2dc
Victoria Hale <vic.hale74@gmail.com>
2019-10-30 16:17:26 +0000

    modified lib

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,27 +5,20 @@ require 'open-uri'
 class ExtraordinaryFinds::CLI
   def call 
     puts "Today's Extraordinary Finds:"
-    # list_finds
-    # menu
+    print_finds
+    menu
   
   end
   
-#   def list_finds
-#     puts 
-#     1.
-#     2.
-#     3.
-#     4.
-#     5.
-#     6.
-#     7.
-#     8.
-#     9.
-#     10.
-#   end
-#   def menu
-#     puts "Enter the number of the find you'd like more information for. Type list to see the list again, or exit."
-#     #some code here to reposnd to user input
-#   end 
+  def print_finds
+    Finds.all.each.with_index(1) do |index, title|
+      puts "#{index}". "#{title}"
+    end
+  end
+  
+  def menu
+    puts "Enter the number of the find you'd like more information for. Type print to see the list again, or exit."
+    #some code here to reposnd to user input
+  end 
   
 # end
--- lib/finds.rb
+++ lib/finds.rb
@@ -1,4 +1,12 @@
 class ExtraordinaryFinds::Finds
   attr_accessor: :title, :price, :rating
+  @@all = []
   
+  def initialize 
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
 end
